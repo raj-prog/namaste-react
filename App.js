af1b704b4aff8570b812f7dedfefbcc6017681d0
Raju Chougale <rjchgl08@gmail.com>
2024-06-21 20:15:13 +0530

    episode-1

---
+++ App.js
@@ -0,0 +1,31 @@
+/**
+ *
+ * <div class="parent">
+ *      <div class="child">
+ *          <h1>I'm h1 tag</h1>
+ *          <h2>I'm h2 tag</h2>
+ *      </div>
+ *      <div class="child2">
+ *          <h1>I'm h1 tag</h1>
+ *          <h2>I'm h2 tag</h2>
+ *      </div>
+ * </div>
+ *
+ * ReactElement(Object) => HTML(Browser understands)
+ *
+ */
+
+const parent = React.createElement("div", { class: "parent" }, [
+  React.createElement("div", { class: "child" }, [
+    React.createElement("h1", {}, "I'm h1 tag"),
+    React.createElement("h2", {}, "I'm h2 tag"),
+  ]),
+  React.createElement("div", { class: "child2" }, [
+    React.createElement("h1", {}, "I'm h1 tag"),
+    React.createElement("h2", {}, "I'm h2 tag"),
+  ]),
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
