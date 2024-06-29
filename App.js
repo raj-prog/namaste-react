7c74e61ed6c011e1a87c551563b0eeb90b2757bc
Raju Chougale <rjchgl08@gmail.com>
2024-06-29 13:16:51 +0530

    episode-3

--- App.js
+++ App.js
@@ -1,34 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-/**
- *
- * <div class="parent">
- *      <div class="child">
- *          <h1>I'm h1 tag</h1>
- *          <h2>I'm h2 tag</h2>
- *      </div>
- *      <div class="child2">
- *          <h1>I'm h1 tag</h1>
- *          <h2>I'm h2 tag</h2>
- *      </div>
- * </div>
- *
- * ReactElement(Object) => HTML(Browser understands)
- *
- */
-
-const parent = React.createElement("div", { className: "parent" }, [
-  React.createElement("div", { className: "child" }, [
-    React.createElement("h1", {}, "I'm h1 tag"),
-    React.createElement("h2", {}, "I'm h2 tag"),
-  ]),
-  React.createElement("div", { className: "child2" }, [
-    React.createElement("h1", {}, "I'm h1 tag"),
-    React.createElement("h2", {}, "I'm h2 tag"),
-  ]),
-]);
+const AppLayout = () => {
+  return <div className="app"></div>;
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(<AppLayout />);
